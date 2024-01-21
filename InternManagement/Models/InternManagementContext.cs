2349c05064fa7180daeda7d1263e3008af161444
khiemtrv <20004072@st.vlute.edu.vn>
2024-01-21 18:37:19 +0700

    tuan 1

---
+++ InternManagement/Models/InternManagementContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InternManagement.Models
+{
+    public class InternManagementContext : DbContext
+    {
+        public InternManagementContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //public DbSet<User> Users { get; set; }
+    }
+}
